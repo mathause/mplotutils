9223246b1d2084e28b38a8458158bc73892e9909
Mathias Hauser <mathause@users.noreply.github.com>
2022-12-23 20:36:14 +0100

    add example to README (#29)

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # mplotutils
 
-helper functions for cartopy and matplotlib
+> *helper functions for cartopy and matplotlib*
 
 This package solves two main problems for plots with maps created with cartopy. Because these plots have a fixed aspect ratio (1) colorbars will extend beyond the visible axes and (2) the distance between individual subplots will seemingly be random.
+
+
+Without mplotutils         |  With mplotutils
+:-------------------------:|:-------------------------:
+<img src="docs/example_no_mpu.png" alt="Without mplotutils" width="250"/> | <img src="docs/example_mpu.png" alt="With mplotutils" width="250"/>
+
+The code to create the examples can be found in [docs/example.py](docs/example.py).
 
 ## Changelog
 
@@ -10,5 +17,4 @@ See [CHANGELOG.md](CHANGELOG.md).
 
 ## History
 
-This package bases on functions developped for the [python visualisation workshop at C2SM](https://github.com/C2SM/pyvis/) (Part 3 -> `utils.py`).
-`utils.py` in turn is based on [mutils](https://github.com/mathause/mutils).
+This package bases on functions developped for the [python visualisation workshop at C2SM](https://github.com/C2SM/pyvis/).
---
+++ docs/example.py
@@ -0,0 +1,66 @@
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+import mplotutils as mpu
+
+
+def plot_map_no_mpu():
+    """plot 2 x 2 global maps _not_ using mplotutils"""
+
+    # create example data
+    lon, lat, data = mpu.sample_data_map(30, 30)
+
+    # create figure and axes
+    f, axs = plt.subplots(2, 2, subplot_kw={"projection": ccrs.Robinson()})
+    axs = axs.flatten()
+
+    # plot data to each axes
+    for ax in axs:
+        mappable = ax.pcolormesh(lon, lat, data, transform=ccrs.PlateCarree())
+        ax.coastlines()
+
+    # add colorbar
+    f.colorbar(mappable=mappable, ax=axs)
+
+    f.suptitle("Without mplotutils")
+
+
+def plot_map_mpu():
+    """plot 2 x 2 global maps using mplotutils"""
+
+    # create example data
+    lon, lat, data = mpu.sample_data_map(30, 30)
+
+    # create figure and axes
+    f, axs = plt.subplots(2, 2, subplot_kw={"projection": ccrs.Robinson()})
+    axs = axs.flatten()
+
+    # plot data to each axes
+    for ax in axs:
+        mappable = ax.pcolormesh(lon, lat, data, transform=ccrs.PlateCarree())
+        ax.coastlines()
+
+    # add colorbar using mplotutils
+    mpu.colorbar(mappable=mappable, ax1=axs[1], ax2=axs[3])
+
+    # adjust the margins manually
+    f.subplots_adjust(left=0.025, right=0.85, top=0.9, bottom=0.05)
+
+    # ensure the figure has the correct size
+    mpu.set_map_layout(axs)
+
+    # call a draw to ensure the colorbar is correct
+    f.canvas.draw()
+
+    f.suptitle("With mplotutils")
+
+
+if __name__ == "__main__":
+
+    opt = {"dpi": 200, "facecolor": "0.9", "transparent": False}
+
+    plot_map_no_mpu()
+    plt.savefig("example_no_mpu.png", **opt)
+
+    plot_map_mpu()
+    plt.savefig("example_mpu.png", **opt)
